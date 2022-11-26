55475357600b5a6bd2ffabff39e7adcf379db5a4
Katrin <CCEBEPA@yandex.ru>
2022-11-26 20:20:22 +0100

    Добавлен второй вариант решения задач calculateVolumeAndArea, getCoupeNumber. Улучшено решение getCoupeNumber

--- js/script.js
+++ js/script.js
@@ -385,6 +385,22 @@ function calculateVolumeAndArea(n) {
 
 }
 
+function calculateVolumeAndArea(length) {
+    if (typeof (length) !== 'number' || length < 0 || !Number.isInteger(length)) {
+        return "При вычислении произошла ошибка";
+    }
+
+    let volume = 0,
+        area = 0;
+
+    volume = length * length * length;
+    // length ** 3 - это тоже самое, что и выше или варианты через цикл.
+    // ** - это оператор степени, напоминаю. Но онлайн редактор его не принимает =/
+    area = 6 * (length * length);
+
+    return `Объем куба: ${volume}, площадь всей поверхности: ${area}`;
+}
+
 // // function calculateVolumeAndArea(result) {
 // //     if (typeof result === Number) {
 // //         return ('Объем куба: ' + String(result*result*result) + ', площадь всей поверхности: ' + String(6*result*result)); 
@@ -437,24 +453,55 @@ function getCoupeNumber(n) {
     } else if (n % 4 === 0) {
         result = n / 4;
         console.log(result)
-    } else if (n % 4 === 1) {
-        result = (n + 3) / 4;
+    } else if (n % 4 !== 0) {
+        result = Math.trunc(n / 4 + 1)
         console.log(result)
-    } else if (n % 4 === 2) {
-        result = (n + 2) / 4;
-        console.log(result)
-    }
-    else if (n % 4 === 3) {
-        result = (n + 1) / 4;
-        console.log(result)
-    }
+    } 
+    // else if (n % 4 === 2) {
+    //     result = (n + 2) / 4;
+    //     console.log(result)
+    // }
+    // else if (n % 4 === 3) {
+    //     result = (n + 1) / 4;
+    //     console.log(result)
+    // }
 }
 
+
+function getCoupeNumber(seatNumber) {
+    if (typeof (seatNumber) !== 'number' || seatNumber < 0 || !Number.isInteger(seatNumber)) {
+        return "Ошибка. Проверьте правильность введенного номера места";
+    }
+
+    if (seatNumber === 0 || seatNumber > 36) {
+        return "Таких мест в вагоне не существует";
+    }
+
+    return Math.ceil(seatNumber / 4);
+    // тут очень много вариантов решения, но выбрал один из элегантных :)
+}
 // getCoupeNumber(33)
 // getCoupeNumber(7)
 // getCoupeNumber(300)
-getCoupeNumber(0)
+// getCoupeNumber(0)
 // getCoupeNumber(7.7)
-getCoupeNumber(-10)
+// getCoupeNumber(-10)
 // getCoupeNumber('Hello')
+
+
+
+
+
+function getCoupeNumber(seatNumber) {
+    if (typeof (seatNumber) !== 'number' || seatNumber < 0 || !Number.isInteger(seatNumber)) {
+        return "Ошибка. Проверьте правильность введенного номера места";
+    }
+
+    if (seatNumber === 0 || seatNumber > 36) {
+        return "Таких мест в вагоне не существует";
+    }
+
+    return Math.ceil(seatNumber / 4);
+    // тут очень много вариантов решения, но выбрал один из элегантных :)
+}
 
