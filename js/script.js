36403a407962cfab6b98ba125ab8080922251762
Katrin <CCEBEPA@yandex.ru>
2022-11-30 18:14:48 +0100

    Tasks for working with objects var.3

--- js/script.js
+++ js/script.js
@@ -681,28 +681,6 @@ function findMaxNumber(first, second, third, fourth) {
 
 // P.S. желательно использовать деструктуризацию, но не обязательно
 
-// function showExperience (plan) {
-
-//     const {exp} = plan.skills;
-
-//     console.log(exp);
-//     return exp;
-// };
-
-// showExperience(personalPlanPeter);
-
-// 2) Напишите функцию showProgrammingLangs, которая будет принимать в себя объект со всеми данными и возвращать строку в нужном виде.
-
-// Пример:
-
-// showProgrammingLangs(personalPlanPeter)  =>
-
-// "Язык js изучен на 20% Язык php изучен на 10%"
-
-// Причем функция должна работать вне зависимости от количества языков. Если ни один не указан, то возвращается пустая строка.
-
-// P.S. Для переноса строки используется \n в конце строки.
-
 const personalPlanPeter = {
     name: "Peter",
     age: "29",
@@ -724,28 +702,90 @@ const personalPlanPeter = {
     }
 };
 
+// const personalPlanPeter = {
+//     name: "Peter",
+//     age: "29",
+//     skills: {
+//         languages: ['ru', 'eng'],
+//         programmingLangs: {
+//             js: '20%',
+//             php: '10%'
+//         },
+//         exp: '1 month'
+//     },
+//     showAgeAndLangs: function(plan) {
+//         const {age} = plan;
+//         const {languages} = plan.skills;
+//         let str = `Мне ${age} и я владею языками: `;
 
-function showProgrammingLangs(plan) {
+//         languages.forEach(function(lang) {
+//             str += `${lang.toUpperCase()} `;
+//         });
 
-    const programming = Object.keys(plan.skills.programmingLangs);
-    let result = "";
+//         return str;
+//     }
+// };
 
-    for (let i = 0; i < programming.length; i++) {
-        console.log(programming)
-        console.log(i)
-        if (programming[i] === undefined) {
-            console.log(``);
-            result = result + '';
-        } else {
-            console.log(`Язык ${programming[i]} изучен на ${20}\n`);
-            result = result + `Язык ${programming[i]} изучен на ${20}\n`;
-        } 
-    }
+// function showExperience (plan) {
 
-    return result;
-}
+//     const {exp} = plan.skills;
 
-showProgrammingLangs(personalPlanPeter)
+//     console.log(exp);
+//     return exp;
+// };
+
+// showExperience(personalPlanPeter);
+
+
+
+// 2) Напишите функцию showProgrammingLangs, которая будет принимать в себя объект со всеми данными и возвращать строку в нужном виде.
+
+// Пример:
+
+// showProgrammingLangs(personalPlanPeter)  =>
+
+// "Язык js изучен на 20% Язык php изучен на 10%"
+
+// Причем функция должна работать вне зависимости от количества языков. Если ни один не указан, то возвращается пустая строка.
+
+// P.S. Для переноса строки используется \n в конце строки.
+
+
+// function showProgrammingLangs(plan) {
+
+//     const programming = Object.keys(plan.skills.programmingLangs);
+//     let result = "";
+
+//     for (let i = 0; i < programming.length; i++) {
+      
+//         if (programming[i] === undefined) {
+//             console.log(``);
+//             result = result + '';
+//         } else {
+//             let percent = 
+//             console.log(programming[i]);
+//             console.log(`Язык ${programming[i]} изучен на ${percent}\n`);
+//             result = result + `Язык ${programming[i]} изучен на ${percent}\n`;
+//         } 
+//     }
+
+//     return result;
+    
+// }
+
+// showExperience(personalPlanPeter);
+
+// function showProgrammingLangs(plan) {
+//     let str = '';
+//     const {programmingLangs} = plan.skills;
+//     for (let key in programmingLangs) {
+//         str += `Язык ${key} изучен на ${programmingLangs[key]}\n`
+//     }
+
+//     return str;
+// }
+
+// showProgrammingLangs(personalPlanPeter)
 
 // 3) Создайте метод showAgeAndLangs внутри объекта personalPlanPeter. При его вызове метод будет принимать в себя объект и возвращать строку в нужном виде.
 
@@ -756,3 +796,5 @@ showProgrammingLangs(personalPlanPeter)
 
 // Заметьте, что возраст и языки подставляются автоматически из объекта, 
 // а языки всегда в верхнем регистре (большими буквами). Если данные в объекте поменяются, то и сообщение тоже изменится.
+
+
